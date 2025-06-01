33854f7492c7fef53abddad714256a4f2d1cfb9c
chanvkuljit <kuldeepdave41@gmail.com>
2025-06-02 03:05:00 +0530

    fist commit

---
+++ Socket_Oneway/server.java
@@ -0,0 +1,43 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class server {
+  
+    public static void main(String[] args) {
+        try (ServerSocket ss = new ServerSocket(5000)) {
+            System.out.println("Server started");
+            
+            while (true) {
+                System.out.println("Waiting for a client...");
+                Socket s = ss.accept();
+                System.out.println("Client accepted: " + s.getInetAddress());
+                
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                Scanner sc = new Scanner(System.in);
+                
+                String clientMessage = "";
+                
+                while (true) {
+                    clientMessage = dis.readUTF();
+                    System.out.println("Client: " + clientMessage);
+                    
+                    if (clientMessage.equalsIgnoreCase("Over")) {
+                        System.out.println("Client disconnected");
+                        break;
+                    }
+                }
+                
+                s.close();
+                sc.close();
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
